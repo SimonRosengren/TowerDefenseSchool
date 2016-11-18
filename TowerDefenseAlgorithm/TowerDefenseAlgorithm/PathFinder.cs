d94521a7598e96da17baecc53760c1681f2197fb
LuddeW <ludwig.wiktorsson@gmail.com>
2016-11-18 14:27:26 +0100

    changes

--- TowerDefenseAlgorithm/TowerDefenseAlgorithm/PathFinder.cs
+++ TowerDefenseAlgorithm/TowerDefenseAlgorithm/PathFinder.cs
@@ -18,10 +18,6 @@ namespace TowerDefenseAlgorithm
         // Holds the nodes that have already been searched.
         public static Stack<PathNode> closedStack = new Stack<PathNode>();
         
-
-        /// <summary>
-        /// Creates the map with passable and not passable nodes
-        /// </summary>
         public static void CreateMap()
         {
             for (int i = 0; i < Globals.X_SIZE; i++)
@@ -38,9 +34,7 @@ namespace TowerDefenseAlgorithm
             }
             SetNeighbours();
         }
-        /// <summary>
-        /// Setting the passable neighbours for each node in the map
-        /// </summary>
+
         public static void SetNeighbours()
         {
             for (int i = 1; i < Globals.X_SIZE - 1; i++) //Börjar på 1 för att skippa kanterna
@@ -102,10 +96,7 @@ namespace TowerDefenseAlgorithm
                 }
             }
         }
-        /// <summary>
-        /// Chescks if therw will be a possible path between two nodes, if there is one sets the path-list to this path.
-        /// </summary>
-        /// <returns></returns>
+
         public static bool CalculateClosestPath()
         {
             if (FindPath(new Point(1, 2), new Point(12, 13)).Count != 0) //Finns det en path?
@@ -116,18 +107,12 @@ namespace TowerDefenseAlgorithm
             return false;   //Returnar false om ingen pathg går att hitta
         }
 
-
-        /// <summary>
-        /// Calculates H, a estimated path between two points
-        /// </summary>
         public static float Heuristic(Point point1, Point point2)
         {
             return Math.Abs(point1.X - point2.X) +
                    Math.Abs(point1.Y - point2.Y);
         }
-        /// <summary>
-        /// Resets the state of the search nodes.
-        /// </summary>
+
         public static void ResetSearchNodes()
         {
             openList.Clear();
@@ -154,9 +139,7 @@ namespace TowerDefenseAlgorithm
                 }
             }
         }
-        /// <summary>
-        /// Returns the node with the smallest distance to goal.
-        /// </summary>
+
         private static PathNode FindBestNode()
         {
             PathNode currentTile = openList[0];
@@ -174,18 +157,13 @@ namespace TowerDefenseAlgorithm
             }
             return currentTile;
         }
-        /// <summary>
-        /// Use the parent field of the search nodes to trace
-        /// a path from the end node to the start node.
-        /// </summary>
+
         private static List<Vector2> FindFinalPath(PathNode startNode, PathNode endNode)
         {
             closedStack.Push(endNode);
 
             PathNode parentTile = endNode.Parent;
 
-            // Trace back through the nodes using the parent fields
-            // to find the best path.
             while (parentTile != startNode)
             {
                 closedStack.Push(parentTile);
@@ -203,9 +181,7 @@ namespace TowerDefenseAlgorithm
             }
             return finalPath;
         }
-        ///<summary>
-        /// Finds the optimal path from one point to another.
-        /// </summary>
+
         public static List<Vector2> FindPath(Point startPoint, Point endPoint)
         {
             // Only try to find a path if the start and end points are different.
@@ -213,24 +189,11 @@ namespace TowerDefenseAlgorithm
             {
                 return new List<Vector2>();
             }
-
-            /////////////////////////////////////////////////////////////////////
-            // Step 1 : Clear the Open and Closed Lists and reset each node’s F 
-            //          and G values in case they are still set from the last 
-            //          time we tried to find a path. 
-            /////////////////////////////////////////////////////////////////////
             ResetSearchNodes();
 
-            // Store references to the start and end nodes for convenience.
             PathNode startNode = map[startPoint.X, startPoint.Y];
             PathNode endNode = map[endPoint.X, endPoint.Y];
 
-            /////////////////////////////////////////////////////////////////////
-            // Step 2 : Set the start node’s G value to 0 and its F value to the 
-            //          estimated distance between the start node and goal node 
-            //          (this is where our H function comes in) and add it to the 
-            //          Open List. 
-            /////////////////////////////////////////////////////////////////////
             startNode.InOpenList = true;
 
             startNode.DistanceToGoal = Heuristic(startPoint, endPoint);
@@ -238,91 +201,52 @@ namespace TowerDefenseAlgorithm
 
             openList.Add(startNode);
 
-            /////////////////////////////////////////////////////////////////////
-            // Setp 3 : While there are still nodes to look at in the Open list : 
-            /////////////////////////////////////////////////////////////////////
             while (openList.Count > 0)
             {
-                /////////////////////////////////////////////////////////////////
-                // a) : Loop through the Open List and find the node that 
-                //      has the smallest F value.
-                /////////////////////////////////////////////////////////////////
+
                 PathNode currentNode = FindBestNode();
 
-                /////////////////////////////////////////////////////////////////
-                // b) : If the Open List empty or no node can be found, 
-                //      no path can be found so the algorithm terminates.
-                /////////////////////////////////////////////////////////////////
                 if (currentNode == null)
                 {
                     break;
                 }
 
-                /////////////////////////////////////////////////////////////////
-                // c) : If the Active Node is the goal node, we will 
-                //      find and return the final path.
-                /////////////////////////////////////////////////////////////////
                 if (currentNode == endNode)
                 {
                     // Trace our path back to the start.
                     return FindFinalPath(startNode, endNode);
                 }
 
-                /////////////////////////////////////////////////////////////////
-                // d) : Else, for each of the Active Node’s neighbours :
-                /////////////////////////////////////////////////////////////////
                 for (int i = 0; i < currentNode.neighbours.Count; i++)
                 {
                     PathNode neighbor = currentNode.neighbours[i];
 
-                    //////////////////////////////////////////////////
-                    // i) : Make sure that the neighbouring node can 
-                    //      be walked across. 
-                    //////////////////////////////////////////////////
                     if (neighbor == null || neighbor.isWall == true)
                     {
                         continue;
                     }
 
-                    //////////////////////////////////////////////////
-                    // ii) Calculate a new G value for the neighbouring node.
-                    //////////////////////////////////////////////////
                     float distanceTraveled = currentNode.DistanceTraveled + 1;
 
                     // An estimate of the distance from this node to the end node.
                     float heuristic = Heuristic(new Point(neighbor.x, neighbor.y), endPoint);
 
-                    //////////////////////////////////////////////////
-                    // iii) If the neighbouring node is not in either the Open 
-                    //      List or the Closed List : 
-                    //////////////////////////////////////////////////
                     if (neighbor.InOpenList == false && neighbor.InClosedList == false)
                     {
-                        // (1) Set the neighbouring node’s G value to the G value 
-                        //     we just calculated.
+
                         neighbor.DistanceTraveled = distanceTraveled;
-                        // (2) Set the neighbouring node’s F value to the new G value + 
-                        //     the estimated distance between the neighbouring node and
-                        //     goal node.
+
                         neighbor.DistanceToGoal = distanceTraveled + heuristic;
-                        // (3) Set the neighbouring node’s Parent property to point at the Active 
-                        //     Node.
+
                         neighbor.Parent = currentNode;
-                        // (4) Add the neighbouring node to the Open List.
+
                         neighbor.InOpenList = true;
                         openList.Add(neighbor);
                     }
-                    //////////////////////////////////////////////////
-                    // iv) Else if the neighbouring node is in either the Open 
-                    //     List or the Closed List :
-                    //////////////////////////////////////////////////
+
                     else if (neighbor.InOpenList || neighbor.InClosedList)
                     {
-                        // (1) If our new G value is less than the neighbouring 
-                        //     node’s G value, we basically do exactly the same 
-                        //     steps as if the nodes are not in the Open and 
-                        //     Closed Lists except we do not need to add this node 
-                        //     the Open List again.
+
                         if (neighbor.DistanceTraveled > distanceTraveled)
                         {
                             neighbor.DistanceTraveled = distanceTraveled;
@@ -333,10 +257,6 @@ namespace TowerDefenseAlgorithm
                     }
                 }
 
-                /////////////////////////////////////////////////////////////////
-                // e) Remove the Active Node from the Open List and add it to the 
-                //    Closed List
-                /////////////////////////////////////////////////////////////////
                 openList.Remove(currentNode);
                 currentNode.InClosedList = true;
             }
